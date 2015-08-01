3f279b66beee77cde5addd6d1a724eb24fff815c
JunerMe <juner@juner.me>
2015-08-01 23:47:25 +0800

    主页v1.0版本完成

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,39 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Juner'
 
+
+
 from flask_wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, ValidationError
+from wtforms.validators import InputRequired, length
+from ..models import SiteModel
 
-
+#首页表单
 class SiteForm(Form):
-    name = StringField('名称',validators=[DataRequired()])
-    site = StringField('网址',validators=[DataRequired()])
+    name = StringField('名称',validators=[InputRequired(), length(1,64)])
+    site = StringField('网址',validators=[InputRequired()])
     submit = SubmitField('添加网址')
+
+
+    def validate_name(self,field):
+        if SiteModel.query.filter_by(name=field.data).first():
+            raise ValidationError('该网址已经被添加了哦')
+
+
+    def validate_site(self,field):
+        if field.data[:4] != 'www.':
+            raise ValidationError('网址应该以“www.”开头！请重新输入')
+        if SiteModel.query.filter_by(site=field.data).first():
+            raise ValidationError('该网站名已经被添加了哦')
+
+
+#网页编辑页面表单
+class EditForm(Form):
+    name = StringField('名称',validators=[length(0,64)])
+    site = StringField('网址')
+    submit = SubmitField('确认更改')
+
+
+    def validate_site(self,field):
+        if field.data[:4] != 'www.':
+            raise ValidationError('网址应该以“www.”开头！请重新输入')
--- app/main/views.py
+++ app/main/views.py
@@ -2,11 +2,12 @@ __author__ = 'Juner'
 
 from . import main
 from flask import render_template, redirect, url_for
-from .forms import SiteForm
+from .forms import SiteForm, EditForm
 from ..models import SiteModel
 from .. import db
 
 
+#主页路由
 @main.route('/', methods=['GET','POST'])
 def index():
     form = SiteForm()
@@ -16,3 +17,32 @@ def index():
         return redirect(url_for('.index'))
     sites = SiteModel.query.all()
     return render_template('index.html',form=form, sites=sites)
+
+
+#编辑网址路由
+@main.route('/edit_site',methods=['GET','POST'])
+def editsite():
+    sites = SiteModel.query.all()
+    return render_template('edit-site.html', sites=sites)
+
+#编辑单个路由
+@main.route('/edit/<int:id>', methods=['GET','POST'])
+def edit_id(id):
+    form = EditForm()
+    site =  SiteModel.query.get(id)
+    if form.validate_on_submit():
+        site.name = form.name.data
+        site.site = form.site.data
+        db.session.add(site)
+        return redirect(url_for('.editsite'))
+    form.name.data = site.name
+    form.site.data = site.site
+    return render_template('edit-id.html', form = form, site=site)
+
+#删除网站
+@main.route('/delete/<int:id>', methods=['GET','POST'])
+def delete_id(id):
+    site =  SiteModel.query.get(id)
+    db.session.delete(site)
+    db.session.commit()
+    return redirect(url_for('.editsite'))
